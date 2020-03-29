59ae0925fa1f4f917f642d759468218063031acb
Jacob Zirbel <jzerble@gmail.com>
2020-03-28 19:10:43 -0500

    add imgURL to questions array

--- assets/javascript/questions.js
+++ assets/javascript/questions.js
@@ -2,41 +2,57 @@ var allQuestions = [
 	{
 		question: "What is the question 1?",
 		correctAnswer: "Correct1",
-		answers: ["wrong", "wrong", "wrong"]
+		answers: ["wrong", "wrong", "wrong"],
+		imgURL:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Socrates_Louvre.jpg/1200px-Socrates_Louvre.jpg"
 	},
 	{
 		question: "What is the question 2?",
 		correctAnswer: "Correct2",
-		answers: ["wrong", "wrong", "wrong"]
+		answers: ["wrong", "wrong", "wrong"],
+		imgURL:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Socrates_Louvre.jpg/1200px-Socrates_Louvre.jpg"
 	},
 	{
 		question: "What is the question 3?",
 		correctAnswer: "Correct3",
-		answers: ["wrong", "wrong", "wrong"]
+		answers: ["wrong", "wrong", "wrong"],
+		imgURL:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Socrates_Louvre.jpg/1200px-Socrates_Louvre.jpg"
 	},
 	{
 		question: "What is the 4question 4?",
 		correctAnswer: "Correct4",
-		answers: ["wrong", "wrong", "wrong"]
+		answers: ["wrong", "wrong", "wrong"],
+		imgURL:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Socrates_Louvre.jpg/1200px-Socrates_Louvre.jpg"
 	},
 	{
 		question: "What is the question 5?",
 		correctAnswer: "Correct5",
-		answers: ["wrong", "wrong", "wrong"]
+		answers: ["wrong", "wrong", "wrong"],
+		imgURL:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Socrates_Louvre.jpg/1200px-Socrates_Louvre.jpg"
 	},
 	{
 		question: "What is the question 6?",
 		correctAnswer: "Correct6",
-		answers: ["wrong", "wrong", "wrong"]
+		answers: ["wrong", "wrong", "wrong"],
+		imgURL:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Socrates_Louvre.jpg/1200px-Socrates_Louvre.jpg"
 	},
 	{
 		question: "What is the question 7?",
 		correctAnswer: "Correct7",
-		answers: ["wrong", "wrong", "wrong"]
+		answers: ["wrong", "wrong", "wrong"],
+		imgURL:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Socrates_Louvre.jpg/1200px-Socrates_Louvre.jpg"
 	},
 	{
 		question: "What is the question 8?",
 		correctAnswer: "Correct8",
-		answers: ["wrong", "wrong", "wrong"]
+		answers: ["wrong", "wrong", "wrong"],
+		imgURL:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Socrates_Louvre.jpg/1200px-Socrates_Louvre.jpg"
 	}
 ];
